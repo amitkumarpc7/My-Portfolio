9a61103dce177ffc97bd075dbd49736c20f1bcda
amitkumarpc7 <amitkpc11@gmail.com>
2024-07-18 22:51:10 +0530

    Updated Projects

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import About from "./Components/About"
 import Experience from "./Components/Experience"
 import Hero from "./Components/Hero"
 import Navbar from "./Components/Navbar"
+import Projects from "./Components/Projects"
 import Technologies from "./Components/Technologies"
 
 
